9cac5ccc8ea687ecd6dd956f7c9860aa902ebfc1
MEC4RNG <pgpolarized@gmail.com>
2026-09-03 12:27:02 -0400

    Add centralized campaign configuration

---
+++ assets/site-config.js
@@ -0,0 +1,11 @@
+
+window.GTD_CONFIG = {
+  campaignStatus: "Pre-Launch",
+  plannedStart: "Early April 2027",
+  trailMilesCompleted: 0,
+  pledgedSupport: 0,
+  donationsReceived: 0,
+  currentLocation: "Pre-Hike",
+  rallyUpUrl: "",
+  contactEmail: "goingthedistancecampaign@gmail.com"
+};
